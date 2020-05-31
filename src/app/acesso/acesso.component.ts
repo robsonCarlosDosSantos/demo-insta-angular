f83d4e87dd70fa149da5ce1ddfdf856c810d5b2c
Robson Carlos <rsc.robson.carlos.santos@gmail.com>
2020-05-31 14:27:30 +0100

    animation login

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -6,24 +6,40 @@ import { trigger, animate, state, transition, style } from '@angular/animations'
   templateUrl: './acesso.component.html',
   styleUrls: ['./acesso.component.css'],
   animations: [
-    trigger('acesso', [
+    trigger('animacao-banner', [
       state('criado',
         style({
           opacity: 1
-        })),
-        transition('void => criado', [
-          style({
-            opacity: 0,
-            transform: 'translate(-50px, 0)'
-          }),
-          animate('500ms 1s ease-in-out')
-        ])
+        })
+      ),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(-50px, 0)'
+        }),
+        animate('500ms 0s ease-in-out')
+      ])
+    ]),
+    trigger('animacao-login', [
+      state('criado',
+        style({
+          opacity: 1
+        })
+      ),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px, 0)'
+        }),
+        animate('500ms 0s ease-in-out')
+      ])
     ])
   ]
 })
 export class AcessoComponent implements OnInit {
 
-  public acesso: string = 'criado';
+  public animacaoBanner: string = 'criado';
+  public animacaoLogin: string = 'criado';
 
   constructor() { }
 
